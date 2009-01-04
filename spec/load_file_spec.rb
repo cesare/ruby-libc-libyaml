925a0ef3297653e45cc430678a7de4f41b3e4bb6
Keiji, Yoshimi <walf443@gmail.com>
2009-01-05 01:20:08 +0900

    remove duplicate test

--- spec/load_file_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), 'spec_helper.rb')
-
-describe YAML::LibYAML do
-  it 'shoud raise exeption in unexist file.' do
-    lambda {
-      YAML::LibYAML.load_file('must_not_exist')
-    }.should raise_error(Errno::ENOENT)
-  end
-end
